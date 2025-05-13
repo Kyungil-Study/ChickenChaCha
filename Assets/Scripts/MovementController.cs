382fbf1f612033684182005b5ec5e5d7cbb536e9
seungDev <55948026+agagtmdtlr@users.noreply.github.com>
2025-05-13 17:34:25 +0900

    test script

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    GameObject particle;
+
+    void Update()
+    {
+        foreach(Touch touch in Input.touches)
+        {
+            if (touch.phase == TouchPhase.Began)
+            {
+                // Construct a ray from the current touch coordinates
+                Ray ray = Camera.main.ScreenPointToRay(touch.position);
+                if (Physics.Raycast(ray, out RaycastHit hitResult))
+                {
+                    // Create a particle if hit
+                    GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    go.transform.position = hitResult.point;
+                }
+            }
+        }
+    }
+}
