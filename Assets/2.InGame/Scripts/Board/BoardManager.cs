41d6d94134e6f0f737f5d510364188d696541a1d
gyusummer <gyusummer@gmail.com>
2025-05-21 18:05:07 +0900

    [게임판 초기화]
    -플레이어 인원수에 따라 올바른 위치에 생성
    -발판 타일과 고르는 타일 랜덤한 이미지 부여 : 12개짜리 가방을 랜덤하게 섞어서 부여

---
+++ Assets/2.InGame/Scripts/Board/BoardManager.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Fusion;
+using UnityEngine;
+
+public class BoardManager : NetworkBehaviour
+{
+    public Tile[] steppingTiles;
+    public Tile[] selectingTiles;
+    public GameObject playerPrefab;
+    
+    private readonly int[] imageKeys = new int[12] {0,1,2,3,4,5,6,7,8,9,10,11};
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            InitTiles();
+        }
+    }
+
+    public void InitTiles()
+    {
+        int[] rndKey = RandomUtil.GetShuffled(imageKeys);
+        int keyLength = imageKeys.Length;
+        
+        // steppingTiles
+        for (int i = 0; i < steppingTiles.Length; i++)
+        {
+            if (i == keyLength)
+            {
+                rndKey = RandomUtil.GetShuffled(imageKeys);
+            }
+            steppingTiles[i].SetImage(rndKey[i % keyLength]);
+            steppingTiles[i].next = steppingTiles[i == steppingTiles.Length - 1 ? 0 : i + 1];
+            steppingTiles[i].prev = steppingTiles[i == 0 ? steppingTiles.Length - 1 : i - 1];
+        }
+        
+        // selectingTiles
+        rndKey = RandomUtil.GetShuffled(imageKeys);
+        for(int i = 0; i < selectingTiles.Length; i++)
+        {
+            selectingTiles[i].SetImage(rndKey[i]);
+        }
+    }
+    
+    public void InitPlayerPiece()
+    {
+        PlayerRef[] players = Runner.ActivePlayers.ToArray();
+        int playerCount = players.Length;
+
+        int div = steppingTiles.Length / playerCount;
+
+        for (int i = 0; i < playerCount; i++)
+        {
+            int pos = i == 0 ? 0 : i * div - 1;
+            Tile tile = steppingTiles[pos];
+            PlayerRef player = players[i];
+            Runner.Spawn(playerPrefab, tile.transform.position, Quaternion.identity, player);
+            tile.player = player;
+        }
+    }
+
+    
+}
