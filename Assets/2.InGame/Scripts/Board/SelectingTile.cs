cb27afa45116eb65bf36a4fc566baadccf2f57f6
mhs0209 <49015763+mhs0209@users.noreply.github.com>
2025-05-22 13:42:51 +0900

    Revert "[룰 매니저]"
    This reverts commit 6258d13dc21b170d85ff47904f5bc8cfa01663be, reversing
    changes made to 29f0db9aa71815396dd03632a53701ba9e8ca0c8.

--- Assets/2.InGame/Scripts/Board/SelectingTile.cs
+++ Assets/2.InGame/Scripts/Board/SelectingTile.cs
@@ -1,23 +1,13 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class SelectingTile : Tile, IPointerClickHandler
 {
-    private static readonly int SHOW_FACE = Animator.StringToHash("ShowFace");
-    
-    public Animator anim;
     public Action<Tile> onClick;
-    
     public void OnPointerClick(PointerEventData eventData)
     {
         onClick?.Invoke(this);
-    }
-
-    public void ShowFace()
-    {
-        anim.SetTrigger(SHOW_FACE);
     }
 }
