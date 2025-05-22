143d47db68fe786ebdf8e99c6708813008d09201
mhs0209 <49015763+mhs0209@users.noreply.github.com>
2025-05-22 11:44:20 +0900

    [룰 매니저]
    맵 매니저의 처리된 요청으로 룰 매니저 코드 보완

--- Assets/2.InGame/Scripts/RuleManager/RuleManager.cs
+++ Assets/2.InGame/Scripts/RuleManager/RuleManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class RuleManager : DontDestroyOnNetwork<RuleManager>, IToRule
 {
+    private Tile mNextTile;
     /*
         할일 목록
         - 1. 플레이어를 앞질렀는지 판별( + 대기 플레이어의 꽁지 여부 확인)
@@ -12,23 +13,38 @@ public class RuleManager : DontDestroyOnNetwork<RuleManager>, IToRule
         - 3. 액티브 플레이어에게 선택 타일을 전달 받고 뒤집어 보여주며 성공 / 실패 판별
     */
     
+    /*
+    public IToMap NextTlieInfo()
+    // A : SteppingTile의 .Next 프로퍼티를 사용하세요.
+    
+    public PlayerRef OnPlayerInfo()
+    // A : SteppingTile의 .StandingPlayer 프로퍼티를 사용하세요.
+
+    public bool EqualTile(IToPlayer tile)
+    // A : Tile의 .IsSamePicture(Tile tile) 메소드를 사용하세요.
+    */
+    
     // 1. 플레이어 앞질렀는지 판별 함수 => 앞질렀는지 여부를 액티브 플레이어에게 전달해줘야됨
     // ps. 앞지른 플레이어가 꽁지를 가지고 있는지도 확인해야 함 => 앞지른 플레이어의 꽁지 정보 받아오기
     public int PassPlayer() // 플레이어를 앞질렀는지 확인하고 앞질렀으면 가져가게 될 꽁지 개수 리턴;
     {
         int tail = 0;
-        IToMap NextTileInfo = null; // 다음 타일 정보
-        if (NextTileInfo.OnPlayerInfo() != null) // 다음 발판의 사람이 있는지 여부
+        SteppingTile tile = null; // 다음 타일 정보
+        while (true)
         {
-            tail += NextTileInfo.OnPlayerInfo().PlayerId/*임시로 지정, 나중에 꼬리 개수를 확인하는 변수로 변환 예정*/;
-            // **꽁지 개수 0으로 변경하는 코드 작성 필요**
-            NextTileInfo = NextTileInfo.NextTlieInfo(); // 있으면 그 다음 발판 확인
+            if (tile.Next.StandingPlayer != null) // 다음 발판의 사람이 있는지 여부
+            {
+                tail += tile.StandingPlayer.PlayerId /*임시로 지정, 나중에 꼬리 개수를 확인하는 변수로 변환 예정*/;
+                //tile.StandingPlayer.PlayerId = 0; // **꽁지 개수 0으로 변경하는 코드 작성 필요**
+                tile = tile.Next; // 있으면 그 다음 발판 확인
+            }
+            else // 위 과정 반복 후 플레이어가 없으면 그 발판 정보 가져오기
+            { 
+                mNextTile = tile.Next;
+                break;
+            }
         }
-        else // 위 과정 반복 후 플레이어가 없으면 그 발판 정보 가져오기
-        {
-            NextTileInfo = null;
-        }
-
+        
         return tail;  // 얻게 될 꽁지 수 리턴
     }
     
@@ -49,9 +65,9 @@ public class RuleManager : DontDestroyOnNetwork<RuleManager>, IToRule
     // ps. 이를 토대로 이동 여부도 판정
     public bool OpenTile()
     {
-        IToMap NextTileInfo = null; //게임매니저에게서 발판 정보 받아오기
-        IToPlayer SelectTileInfo = null; //플레이어에게 선택 타일 정보 받아오기
-        if (NextTileInfo.EqualTile(SelectTileInfo))
+        SteppingTile tile = null; //게임매니저에게서 발판 정보 받아오기
+        Tile SelectTileInfo = null; //플레이어에게 선택 타일 정보 받아오기
+        if (tile.Next.IsSamePicture(SelectTileInfo))
         {
             return true;
         }
