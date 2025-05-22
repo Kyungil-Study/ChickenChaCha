62172c323ca4771258eed0238913fd88e7b3765d
WhiteAppleKo <102124768+WhiteAppleKo@users.noreply.github.com>
2025-05-22 20:12:16 +0900

    Revert "[병합]"
    This reverts commit 021aafb22d6e7190671ab9473a5c95f713df1fa5, reversing
    changes made to 519b5a6c4e7466cb91437af61c7ca204fe1cd21f.

--- Assets/2.InGame/Scripts/Board/SelectingTile.cs
+++ Assets/2.InGame/Scripts/Board/SelectingTile.cs
@@ -1,30 +1,13 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class SelectingTile : Tile, IPointerClickHandler
 {
-    public Animator anim;
-    
-    private static readonly int SHOW_FACE = Animator.StringToHash("ShowFace");
-    private static readonly int HIDE_FACE = Animator.StringToHash("HideFace");
-    
-
     public Action<Tile> onClick;
     public void OnPointerClick(PointerEventData eventData)
     {
         onClick?.Invoke(this);
     }
-    public void ShowFace()
-    {
-        anim.SetTrigger(SHOW_FACE);
-    }
-    
-    public void HideFace()
-    {
-        anim.SetTrigger(HIDE_FACE);
-    }
-
 }
--- Assets/2.InGame/Scripts/Board/SteppingTile.cs
+++ Assets/2.InGame/Scripts/Board/SteppingTile.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class SteppingTile : Tile
 {
-    public SteppingTile Next { get; set; }
-    public SteppingTile Prev { get; set; }
+    public Tile Next { get; set; }
+    public Tile Prev { get; set; }
     public PlayerRef StandingPlayer { get; set; }
 }
