fc8d0f7a629a75148d27d8535a96848c8ba11479
WhiteAppleKo <102124768+WhiteAppleKo@users.noreply.github.com>
2025-05-23 16:38:55 +0900

    [딕셔너리 재정의]

--- Assets/2.InGame/Scripts/Network/GameManager.cs
+++ Assets/2.InGame/Scripts/Network/GameManager.cs
@@ -7,16 +7,7 @@ public class GameManager : DontDestroyOnNetwork<GameManager>, IToNetwork, IPlaye
 {
     
     [Networked] 
-    private NetworkDictionary<int, Tile> mSteppingTileInfo => default;
-
-    public void AddTile(int t, SteppingTile tile)
-    {
-        mSteppingTileInfo.Add(t, tile);
-    }
-    
-    
-    [Networked] 
-    private NetworkDictionary<int, SelectingTile> mSelectingTileInfo => default;
+    private NetworkDictionary<NetworkId, INetworkStruct> mNetworkDictionary => default;
     
     
     public void SendSelectedTile(PlayerRef player, SelectingTile tile)
@@ -35,57 +26,11 @@ public class GameManager : DontDestroyOnNetwork<GameManager>, IToNetwork, IPlaye
         throw new NotImplementedException();
     }
     
-    
-    // 에러 코드 주석 처리
-    // 에러코드 수정후
-    // 아래 파일 호출부도 복원해주세요.
-    // ScoreBoaurUI
-    // PlayerController 
-     [Networked] 
-     private NetworkDictionary<PlayerRef, PlayerInfo> mPlayerInfo => default;
-     
-     
      [Rpc(RpcSources.All, RpcTargets.StateAuthority)]
-     public void RPC_AddPlayer(PlayerRef player)
+     public void RPC_AddDictionary(NetworkId networkId, INetworkStruct networkStruct)
      {
-         if (mPlayerInfo.Count >= 4)
-         {
-             Debug.Log("더 이상 추가 할 수 없습니다.");
-             
-             return;
-         }
-
-         PlayerInfo playerInfo = new PlayerInfo(player, false, 1, new NetworkId());
-         Debug.Log($"{playerInfo.player} , {playerInfo.isActive}, {playerInfo.score}");
-         mPlayerInfo.Add(player, playerInfo);
-     }
-
-     public PlayerInfo? GetPlayerInfoOrNull(PlayerRef player)
-     {
-         if(mPlayerInfo.TryGet(player, out var value))
-         {
-             return value;
-         }
-         return null;
-     }
-
-     public PlayerInfo? GetLocalPlayerOrNull()
-     {
-         if(mPlayerInfo.TryGet(Runner.LocalPlayer, out var value))
-         {
-             return value;
-         }
-         return null;
-     }
-
-     public List<PlayerRef> GetPlayersInfo()
-     {
-         List<PlayerRef> players = new List<PlayerRef>();
-         foreach (var playerInfo in mPlayerInfo)
-         {
-             players.Add(playerInfo.Key);
-         }
-         return players;
+         mNetworkDictionary.Add(networkId, networkStruct);
+         Debug.Log("Added to dictionary: " + networkId);
      }
      
      public void SetPlayerState(PlayerRef Player, bool isActive)
