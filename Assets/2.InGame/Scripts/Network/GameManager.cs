4922ef7dbaf94039a01a8b5e99e3695202dafe60
WhiteAppleKo <102124768+WhiteAppleKo@users.noreply.github.com>
2025-05-24 16:40:52 +0900

    [게임 매니저 수술 중]
    내가 맞춰야 할타일 반환하는거랑 꼬리 뺐기 구현

--- Assets/2.InGame/Scripts/Network/GameManager.cs
+++ Assets/2.InGame/Scripts/Network/GameManager.cs
@@ -28,33 +28,20 @@ public class GameManager : DontDestroyOnNetwork<GameManager>
     }
 
     #region RuleManager
-    // 1. 플레이어 앞질렀는지 판별 함수 => 앞질렀는지 여부를 액티브 플레이어에게 전달해줘야됨
-    // ps. 앞지른 플레이어가 꽁지를 가지고 있는지도 확인해야 함 => 앞지른 플레이어의 꽁지 정보 받아오기
-    public int PassPlayer(SteppingTile tile) // 플레이어를 앞질렀는지 확인하고 앞질렀으면 가져가게 될 꽁지 개수 리턴;
-    {
-        int tail = 0;
-        while (tile.Next.StandingPlayer != PlayerRef.None) // 다음 발판의 사람이 있는지 여부
-        {
-            tail++; /*임시로 지정, 나중에 꼬리 개수를 확인하는 변수로 변환 예정*/;
-            //tile.StandingPlayer.PlayerId = 0; // **꽁지 개수 0으로 변경하는 코드 작성 필요**
-            tile = tile.Next; // 있으면 그 다음 발판 확인
-        }
-        return tail;  // 얻게 될 꽁지 수 리턴
-    }
     
-    // 2. 획득한 꽁지로 승리 판별 => 액티브 플레이어에게 1번의 정보를 리턴 후 꽁지 정보를 받아와 4개일 시 승리 판정 아닐시 게임 진행
+    // 1. 획득한 꽁지로 승리 판별 => 액티브 플레이어에게 1번의 정보를 리턴 후 꽁지 정보를 받아와 4개일 시 승리 판정 아닐시 게임 진행
     // ps. 승리 판정이 나면 다른 플레이어들은 패배 판정
     public bool CheckTail(int tailCount) //이 함수가 true면 승리, false면 진행 혹은 패배
     {
         // 사전 작업: PassPlayer로 꽁지 개수 플레이어에게 전달해주기
-        if (tailCount == 4) // 플레이어 꽁지 개수 확인, 후에 == 4로 변경 예정
+        if (tailCount >= 4) // 플레이어 꽁지 개수 확인, 후에 == 4로 변경 예정
         {
             return true; // 꽁지가 4개면 true 리턴
         }
         return false; //꽁지가 4개 미만이면 false 리턴
     }
     
-    // 3. 액티브 플레이어에게 선택 타일을 전달 받고 뒤집어 보여주며 성공 / 실패 판별 => 액티브 플레이어에게 전달해줘야 됨
+    // 2. 액티브 플레이어에게 선택 타일을 전달 받고 뒤집어 보여주며 성공 / 실패 판별 => 액티브 플레이어에게 전달해줘야 됨
     // ps. 이를 토대로 이동 여부도 판정
     public bool OpenTile(SteppingTile tile, SelectingTile selectTileInfo)
     {
@@ -67,17 +54,34 @@ public class GameManager : DontDestroyOnNetwork<GameManager>
         }
         return false;
     }
-
-    // 4. 뭘 맞춰야 하는지 확인 하는 코드
+    
+    private List<NetworkPlayer> tailPlayers; // 여기 있는 애들한테 꼬리 뺐으면 됌.
+    // 3. 뭘 맞춰야 하는지 확인 하는 코드
     public SteppingTile GetMatchTile(SteppingTile tile)
     {
         while (tile.Next.StandingPlayer != PlayerRef.None) // 다음 발판의 사람이 있는지 여부
         {
+            tailPlayers = new List<NetworkPlayer>();
             var netObj = Runner.GetPlayerObject(tile.Next.StandingPlayer);
+            var netPlayer = netObj.GetComponent<NetworkPlayer>();
+            if (netPlayer.tailCount != 0)
+            {
+                tailPlayers.Add(netPlayer);
+            }
             
             tile = tile.Next; // 있으면 그 다음 발판 확인
         }
         return tile;
+    }
+
+    // 4.  뺏은 꼬리 개수만큼 액티브 플레이어에게 추가
+    public void TakeTails(NetworkPlayer netPlayer)
+    {
+        netPlayer.tailCount += tailPlayers.Count;
+        foreach (var netplayers in tailPlayers)
+        {
+            netplayers.tailCount--;
+        }
     }
     #endregion
 }
