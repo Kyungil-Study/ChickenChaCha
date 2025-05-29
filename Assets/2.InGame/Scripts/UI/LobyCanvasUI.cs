c55a3fd379102bd3a76aeac742c3de12301751a5
briankunkel9 <brainkunkel9@gmail.com>
2025-05-29 16:41:51 +0900

    [로비 유저정보 업데이트 안되는 이슈 수정]
    인게임->로비 올때 유저 정보 업데이트 되도록 수정
    인게임 라이트 방향 변경

--- Assets/2.InGame/Scripts/UI/LobyCanvasUI.cs
+++ Assets/2.InGame/Scripts/UI/LobyCanvasUI.cs
@@ -42,6 +42,7 @@ public class LobyCanvasUI : MonoBehaviour
         {
             mGotoFriendPanel.SetActive(false);
         }
+        userNameText.text = UserManager.Instance.User.NickName;
         UserManager.Instance.OnLoadedUserInfomation += () => { userNameText.text = UserManager.Instance.User.NickName;};
     }
 
